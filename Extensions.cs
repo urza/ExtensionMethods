32f94c8dfe250c26fbbda60b5cb7f3146bf5b6b6
urza <urzidlo@gmail.com>
2013-05-22 12:14:58 +0300

    get random element

--- Extensions.cs
+++ Extensions.cs
@@ -203,6 +203,15 @@ namespace ExtensionMethods
         {
             foreach (var item in @enum) mapFunction(item);
         }
+        
+        public static T GetRandomElement<T>(this IEnumerable<T> list)
+        {
+            // If there are no elements in the collection, return the default value of T
+            if (list.Count() == 0)
+                return default(T);
+ 
+            return list.ElementAt(random.Next(list.Count()));
+        }
     }
     
     public static class ListExtensions
